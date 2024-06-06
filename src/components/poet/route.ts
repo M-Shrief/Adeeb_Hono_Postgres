933adc7e024a7471927bafe3633fc560c5752d8f
M-Shrief <m.shrief.dev@proton.me>
2024-06-06 23:08:53 +0300

    Added Validation for req.params

--- src/components/poet/route.ts
+++ src/components/poet/route.ts
@@ -1,24 +1,34 @@
 import { Hono } from 'hono'
 import { vValidator } from '@hono/valibot-validator'
-import { flatten } from 'valibot'
+import { flatten, object } from 'valibot'
 // Component
 import { PoetService } from './service'
 import { ERROR_MSG, PoetType } from './interface'
 import { createSchema, updateSchema } from './schema'
 // Utils
 import HttpStatusCode from '../../utils/httpStatusCode'
+import { idSchema } from '../../utils/schemas'
 
 export const poetRoute = new Hono()
 
 poetRoute.get('/', async (c) => {
     const poets = await PoetService.getAll();
+    if(!poets) return c.json(ERROR_MSG.NOT_AVAILABLE, HttpStatusCode.NOT_FOUND)
     return c.json(poets, HttpStatusCode.OK)
 })
 
-poetRoute.get('/:id', async (c) => {
-    const poet = await PoetService.getOne(c.req.param('id'))
-    return c.json(poet, HttpStatusCode.OK)
-})
+poetRoute.get(
+    '/:id',
+    vValidator('param', object({id: idSchema}), (result, c) => {
+        if (!result.success) {
+            return c.json({success: result.success, errors: flatten(result.issues).nested}, HttpStatusCode.NOT_ACCEPTABLE)
+        }
+    }),
+    async (c) => {
+        const poet = await PoetService.getOne(c.req.param('id'))
+        return c.json(poet, HttpStatusCode.OK)
+    }
+)
 
 poetRoute.post(
     '/', 
@@ -30,6 +40,7 @@ poetRoute.post(
     async (c) => {
         const poetData = await c.req.json()
         const newPoet = await PoetService.post(poetData as PoetType) 
+        if(!newPoet) c.json(ERROR_MSG.NOT_VALID, HttpStatusCode.NOT_ACCEPTABLE)
         return c.json(newPoet, HttpStatusCode.CREATED)
     }
 )
@@ -38,6 +49,11 @@ poetRoute.post(
 
 poetRoute.put(
     '/:id',
+    vValidator('param', object({id: idSchema}), (result, c) => {
+        if (!result.success) {
+            return c.json({success: result.success, errors: flatten(result.issues).nested}, HttpStatusCode.NOT_ACCEPTABLE)
+        }
+    }),
     vValidator('json', updateSchema, (result, c) => {
         if (!result.success) {
             return c.json({success: result.success, errors: flatten(result.issues).nested}, HttpStatusCode.NOT_ACCEPTABLE)
@@ -46,13 +62,23 @@ poetRoute.put(
     async(c) => {
         const newData = await c.req.json()
         const newPoet = await PoetService.update(c.req.param('id'), newData)
+        if(!newPoet) c.json(ERROR_MSG.NOT_VALID, HttpStatusCode.NOT_ACCEPTABLE)
         return c.json(newPoet, HttpStatusCode.ACCEPTED)
     }
 )
 
-poetRoute.delete('/:id', async(c) => {
-    const deletedPoet = await PoetService.delete(c.req.param('id'))
-    return c.json(deletedPoet, HttpStatusCode.ACCEPTED)}
+poetRoute.delete(
+    '/:id',
+    vValidator('param', object({id: idSchema}), (result, c) => {
+        if (!result.success) {
+            return c.json({success: result.success, errors: flatten(result.issues).nested}, HttpStatusCode.NOT_ACCEPTABLE)
+        }
+    }),
+    async(c) => {
+        const deletedPoet = await PoetService.delete(c.req.param('id'))
+        if(!deletedPoet) c.json(ERROR_MSG.NOT_FOUND, HttpStatusCode.NOT_ACCEPTABLE)
+        return c.json(deletedPoet, HttpStatusCode.ACCEPTED)
+    }
 )
 
 
---
+++ src/utils/schemas.ts
@@ -0,0 +1,3 @@
+import { length, object, pipe, string } from 'valibot'
+
+export const idSchema = pipe(string(), length(24))
