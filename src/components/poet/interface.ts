9bcf1874bd8fc4b7c441f51735db7dd9518ab3b8
M-Shrief <m.shrief.dev@proton.me>
2024-06-06 23:11:24 +0300

    Removed poet.properties msg for ERROR_MSG.

--- src/components/poet/interface.ts
+++ src/components/poet/interface.ts
@@ -11,9 +11,4 @@ export enum ERROR_MSG {
   NOT_AVAILABLE = 'No poet available',
   NOT_FOUND = "Poet's not found",
   NOT_VALID = 'Data for poet is not valid',
-  // Inner Properties
-  NAME = 'name should be letters, and max 50 letters length',
-  TIME_PERIOD = 'time_period should be letters, and max 50 letters length',
-  BIO = 'bio should be letters, and max 300 letters length',
-  REVIEWED = 'reviewed should be true or false',
 }
