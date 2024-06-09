bc2917a613b5b2c7acd94d7fdf0902e1aa6dbdd9
M-Shrief <m.shrief.dev@proton.me>
2024-06-09 14:32:24 +0300

    Added Partner component's repository.

---
+++ src/components/partner/repository.ts
@@ -0,0 +1,32 @@
+import Partner from './model';
+import { PartnerType } from './interface';
+
+export const PartnerDB = {
+  async getInfo(id: string): Promise<PartnerType | null> {
+    return await Partner.findById(id, {
+      name: 1,
+      address: 1,
+      phone: 1,
+    });
+  },
+
+  async signup(partnerData: PartnerType): Promise<PartnerType> {
+    const partner = new Partner({ ...partnerData });
+    return await partner.save();
+  },
+
+  async login(phone: string): Promise<PartnerType | null> {
+    return await Partner.findOne({ phone }, { name: 1, phone: 1, password: 1 });
+  },
+
+  async update(
+    id: string,
+    partnerData: PartnerType,
+  ): Promise<PartnerType | null> {
+    return await Partner.findByIdAndUpdate(id, { $set: partnerData }, {new: true});
+  },
+
+  async delete(id: string): Promise<PartnerType | null> {
+    return await Partner.findByIdAndDelete(id);
+  },
+};
