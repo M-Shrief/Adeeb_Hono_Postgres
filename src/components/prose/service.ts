f6534784557e21c45970271ae774d79ce16af636
M-Shrief <m.shrief.dev@proton.me>
2024-06-09 09:37:26 +0300

    Added Prose component's service.

---
+++ src/components/prose/service.ts
@@ -0,0 +1,49 @@
+// Component
+import {ProseDB} from './repository'
+import { ProseType } from './interface';
+
+export const ProseService = {
+  async getAllWithPoet(): Promise<ProseType[] | false> {
+    const proses = await ProseDB.getAllWithPoet()
+    if (proses.length === 0) return false;
+    return proses;
+  },
+
+  async getRandomWithPoet(num: number): Promise<ProseType[] | false> {
+    const proses = await ProseDB.getRandomWithPoet(num)
+    if (proses.length === 0) return false;
+    return proses;
+  },
+
+  async getOneWithPoet(id: string): Promise<ProseType | false> {
+    const prose = await ProseDB.getOneWithPoet(id)
+    if (!prose) return false;
+    return prose;
+  },
+
+  async post(proseData: ProseType): Promise<ProseType | false> {
+    const newProse = await ProseDB.post(proseData);
+    if (!newProse) return false;
+    return newProse;
+  },
+
+  async postMany(
+    prosesData: ProseType[],
+  ): Promise< ProseType[] | false> {
+    const newProses = await ProseDB.postMany(prosesData);
+    if (newProses.length == 0) return false;
+    return newProses;
+  },
+
+  async update(id: string, proseData: ProseType): Promise<ProseType | false> {
+    const newProse = await ProseDB.update(id, proseData);
+    if (!newProse) return false;
+    return newProse;
+  },
+
+  async delete(id: string) {
+    const prose = await ProseDB.delete(id);
+    if (!prose) return false;
+    return prose;
+  },
+};
