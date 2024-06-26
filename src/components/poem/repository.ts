59b406aafa27fa19da83fc3ccbe3048f6b941192
M-Shrief <m.shrief.dev@proton.me>
2024-06-26 18:59:49 +0300

    Modified Poem's repository to use with Postgres, renamed PoemRedis into PoemCache.

--- src/components/poem/repository.ts
+++ src/components/poem/repository.ts
@@ -1,63 +1,137 @@
-import { Poem } from './model';
-import { cacheClient } from '../../cache';
-import { PoemType } from './interface';
+// Database
+import { AppDataSource } from '../../db';
+// Redis
+import {cacheClient} from '../../cache';
+// Entities
+import { Poem } from './entity';
+// Utils
+import { logger } from '../../utils/logger';
+// Types
+import { DeleteResult, UpdateResult } from 'typeorm';
+import { Logger } from 'winston';
 
+const db = AppDataSource.getRepository(Poem);
+
+/**
+ * Used to access Database's Poem repository.
+ */
 export const PoemDB = {
-  async getAllWithPoet(): Promise<PoemType[]> {
-    return await Poem.find({}, { intro: 1, poet: 1, reviewed: 1 }).populate(
-      'poet',
-      'name',
-    );
-  },
-
-  async getOneWithPoet(id: string): Promise<PoemType | null> {
-    return await Poem.findById(id, {
-      intro: 1,
-      poet: 1,
-      verses: 1,
-      reviewed: 1,
-    }).populate('poet', ['name', 'bio', 'time_period']);
-  },
-
-  async post(poemData: PoemType): Promise<PoemType> {
-    const poem = new Poem({
-      intro: poemData.intro,
-      poet: poemData.poet,
-      verses: poemData.verses,
-      reviewed: poemData.reviewed,
+  /**
+   * Returns an array of Poems with the poet name
+   * @returns
+   */
+  async getAllWithPoet(): Promise<Poem[]> {
+    return await db.find({
+      select: {
+        id: true,
+        intro: true,
+        reviewed: true,
+        poet: {
+          id: true,
+          name: true,
+        },
+      },
+      relations: { poet: true },
+      cache: true, // Default cache lifetime is equal to 1000 ms, this means that if users open the user page 150 times within 3 seconds, only three queries will be executed
     });
-    const newPoem = await poem.save();
-    return newPoem;
   },
-
-  async postMany(poemsData: PoemType[]): Promise<PoemType[]> {
-    return await Poem.insertMany(poemsData);
+  /**
+   * Returns Poem data and its poet data
+   * @param {string} id - poem's id
+   * @returns
+   */
+  async getOneWithPoet(id: string): Promise<Poem | null> {
+    return await db.findOne({
+      where: { id },
+      select: {
+        id: true,
+        intro: true,
+        verses: true,
+        reviewed: true,
+        poet: {
+          id: true,
+          name: true,
+          time_period: true,
+          bio: true,
+        },
+      },
+      relations: { poet: true },
+      cache: true, // Default cache lifetime is equal to 1000 ms, this means that if users open the user page 150 times within 3 seconds, only three queries will be executed
+    });
   },
-
-  async update(id: string, poemData: PoemType): Promise<PoemType | null> {
-    return await Poem.findByIdAndUpdate(id, { $set: poemData }, { new: true });
+  /**
+   * Create a new Poem
+   * @param {Poem} poemData - poem's data
+   * @returns
+   */
+  async post(poemData: Poem): Promise<Poem> {
+    return await db.save(poemData);
   },
-
-  async delete(id: string): Promise<PoemType | null> {
-    return await Poem.findByIdAndDelete(id);
+  /**
+   * Create new Poems
+   * @param {Poem[]} poemsData - poem's data
+   * @returns
+   */
+  async postMany(poemsData: Poem[]): Promise<Poem[]> {
+    return await db.save(poemsData);
+  },
+  /**
+   * Update a Poem's data
+   * @param {string} id - poem's id
+   * @param {Poem} poemData - poem's data
+   * @returns
+   */
+  async update(id: string, poemData: Poem): Promise<UpdateResult> {
+    const result = await db.update(id, poemData);
+    logger.info(result);
+    return result;
+  },
+  /**
+   * Remove a Poem
+   * @param {string} id - poem's id
+   * @returns
+   */
+  async delete(id: string): Promise<DeleteResult> {
+    return await db.delete(id);
   },
 };
 
-export const PoemRedis = {
+/**
+ * control poems' data in cache
+ */
+export const PoemCache = {
+  /**
+   * get a poem from cache
+   * @param {string} id - poem's id
+   * @returns
+   */
   async get(id: string): Promise<string | null> {
     return await cacheClient.get(`poem:${id}`);
   },
-  async set(id: string, poem: PoemType): Promise<string | null> {
-    return await cacheClient.set(
-      `poem:${id}`,
-      JSON.stringify(poem),
-      'EX',
-      60 * 15,
-    );
+  /**
+   * set a poem to cache
+   * @param {string} id - poem's id
+   * @param {Poem} poem - poem's data
+   * @returns
+   */
+  async set(id: string, poem: Poem): Promise<string | Logger | null> {
+    return await cacheClient
+      .set(`poem:${id}`, JSON.stringify(poem), "EX", 60 * 15)
+      .catch((err) => logger.error(`CacheError: couldn't cache poem:${id}`));
   },
+  /**
+   * check if a poem exists
+   * @param {string} id - poem's id
+   * @returns
+   */
   async exists(id: string): Promise<number> {
     return await cacheClient.exists(`poem:${id}`);
   },
+  /**
+   * delete a poem from cache
+   * @param {string} id - poem's id
+   * @returns
+   */
   async delete(id: string): Promise<number> {
     return await cacheClient.del(`poem:${id}`);
   },
